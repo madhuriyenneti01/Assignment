8349524f6747da020f1a8bd791c28e5cf141f573
madhuriyenneti01 <158063578+madhuriyenneti01@users.noreply.github.com>
2024-02-15 13:13:41 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+let Username = document.getElementById("Username");
+let Password = document.getElementById("Password");
+let ConfirmPassword = document.getElementById("ConfirmPassword");
+function execute() {
+
+        if(ConfirmPassword.value === "1234") {
+    alert("Correct Password!!");
+        }
+
+    else{
+        alert("Incorrect Password");
+
+    }
+
+        }
+    
